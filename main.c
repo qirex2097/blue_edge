7218d261e67065c1f7ce28624fd820e0595e022b
Masaru Hori <qirex_rr@yahoo.co.jp>
2025-08-16 20:24:33 +0900

    feat: Display debug counter on window

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <mlx.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 // mlxポインタをスレッドに渡すための構造体
 typedef struct s_data
@@ -10,12 +12,12 @@ typedef struct s_data
 	void	*win;
 	size_t	counter;
 	size_t  prev_counter;
-}			t_data;
+}           t_data;
 
 // スレッドで実行されるカウンター関数
-void	*counter_thread(void *arg)
+void    *counter_thread(void *arg)
 {
-	t_data	*data = (t_data *)arg;
+	t_data  *data = (t_data *)arg;
 
 	data->counter = 0;
 	while (1) {
@@ -29,17 +31,35 @@ void	*counter_thread(void *arg)
 int render_next_frame(void* arg)
 {
 	t_data *data = (t_data*)arg;
+	char buffer[256];
+
 	if (data->counter != data->prev_counter) {
+		// 画面をクリア
+		mlx_clear_window(data->mlx, data->win);
+		// 表示する文字列を作成
+		sprintf(buffer, "Counter: %ld", data->counter);
+		// 文字列をウィンドウに描画
+		mlx_string_put(data->mlx, data->win, 10, 20, 0xFFFFFF, buffer); // 白色で表示
 		printf("render_next_frame: %ld\n", data->counter);
 		data->prev_counter = data->counter;
 	}
 	return 0;
 }
 
+int	key_press_hook(int keycode, t_data *data)
+{
+	if (keycode == 65307) // 65307 is the keycode for ESC
+	{
+		mlx_destroy_window(data->mlx, data->win);
+		exit(0);
+	}
+	return (0);
+}
+
 int	main(void)
 {
-	t_data		data;
-	pthread_t	thread_id;
+	t_data      data;
+	pthread_t   thread_id;
 
 	// minilibxの初期化
 	data.mlx = mlx_init();
@@ -59,6 +79,7 @@ int	main(void)
 	pthread_detach(thread_id);
 	// イベントループ開始
 	mlx_loop_hook(data.mlx, render_next_frame, (void*)&data);
+	mlx_key_hook(data.win, key_press_hook, &data);
 	// counter_threadでmlx_loop_endが呼ばれると、このループは終了する
 	mlx_loop(data.mlx);
 	// mlxのリソースを解放
